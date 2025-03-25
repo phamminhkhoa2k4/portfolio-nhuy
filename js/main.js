89fa7fe9eec07be06c9d0a2169b43208e0b30d62
pham minh khoa <khoapham280@gmail.com>
2025-03-25 21:16:18 +0700

    v1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
 document.addEventListener("scroll", function () {
   const header = document.querySelector("header");
+  const headerContainer = document.querySelector(".header-container");
+  
   const navItems = header.querySelectorAll("nav a");
   const button = header.querySelector("nav .getintouch");
   const logo = header.querySelector("header #logo span");
 
-  if (window.scrollY >= 790) {
+  if (window.scrollY >= 790 ) {
     navItems.forEach((item) => {
-      item.style.color = "black"; 
+      item.style.color = "black";
     });
-    button.style.color = "white"; 
-    button.style.backgroundColor = "#b06c78"; 
+    headerContainer.style.backgroundColor = "#fef3e5";
+    header.style.backgroundColor = "#fef3e5";
+    button.style.color = "white";
+    button.style.backgroundColor = "#b06c78";
     logo.style.color = "black";
   } else {
     navItems.forEach((item) => {
-      item.style.color = ""; 
+      item.style.color = "";
     });
-    button.style.color = "#000"; 
-    button.style.backgroundColor = ""; 
+     headerContainer.style.backgroundColor = "";
+     header.style.backgroundColor = "";
+    button.style.color = "#000";
+    button.style.backgroundColor = "";
     logo.style.color = "";
   }
 });
